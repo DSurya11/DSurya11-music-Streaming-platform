c3d3cb90dc8482b43e605f72d2937eb4e483985e
Surya <suryadantuluri2005@gmail.com>
2025-01-07 16:58:32 +0530

    this is my music streaming platform

---
+++ Database/music.sql
@@ -0,0 +1,239 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 23, 2024 at 11:07 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `music-streaming-platform`
+--
+CREATE DATABASE IF NOT EXISTS `music-streaming-platform` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `music-streaming-platform`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `albums`
+--
+
+CREATE TABLE `albums` (
+  `album_id` int(20) NOT NULL,
+  `album_name` varchar(25) NOT NULL,
+  `artist_id` int(20) NOT NULL,
+  `album_img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+CREATE TABLE `artist` (
+  `artist_id` int(200) NOT NULL,
+  `artist_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+--
+-- Table structure for table `liked_songs`
+--
+
+CREATE TABLE `liked_songs` (
+  `user_id` int(11) NOT NULL,
+  `song_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+--
+-- Table structure for table `playlists`
+--
+
+CREATE TABLE `playlists` (
+  `playlist_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `playlist_name` varchar(60) NOT NULL,
+  `playlist_cover` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+--
+-- Table structure for table `playlist_songs`
+--
+
+CREATE TABLE `playlist_songs` (
+  `playlist_id` int(11) NOT NULL,
+  `song_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+--
+-- Table structure for table `song`
+--
+
+CREATE TABLE `song` (
+  `song_id` int(200) NOT NULL,
+  `song_name` varchar(40) DEFAULT NULL,
+  `genre` varchar(20) NOT NULL,
+  `duration` int(255) UNSIGNED NOT NULL,
+  `song_path` varchar(300) NOT NULL,
+  `image_path` varchar(300) NOT NULL,
+  `artist_id` int(200) NOT NULL,
+  `album_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(8) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `pass` varchar(20) NOT NULL,
+  `display_name` varchar(60) NOT NULL DEFAULT 'Display Name'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `albums`
+--
+ALTER TABLE `albums`
+  ADD PRIMARY KEY (`album_id`),
+  ADD KEY `album_artist_id` (`artist_id`);
+
+--
+-- Indexes for table `artist`
+--
+ALTER TABLE `artist`
+  ADD PRIMARY KEY (`artist_id`);
+
+--
+-- Indexes for table `liked_songs`
+--
+ALTER TABLE `liked_songs`
+  ADD KEY `user_ids` (`user_id`),
+  ADD KEY `song_ids` (`song_id`);
+
+--
+-- Indexes for table `playlists`
+--
+ALTER TABLE `playlists`
+  ADD PRIMARY KEY (`playlist_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `playlist_songs`
+--
+ALTER TABLE `playlist_songs`
+  ADD KEY `playlist_id` (`playlist_id`),
+  ADD KEY `song_id` (`song_id`);
+
+--
+-- Indexes for table `song`
+--
+ALTER TABLE `song`
+  ADD PRIMARY KEY (`song_id`),
+  ADD KEY `artist_id` (`artist_id`),
+  ADD KEY `album_id` (`album_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `albums`
+--
+ALTER TABLE `albums`
+  MODIFY `album_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `artist`
+--
+ALTER TABLE `artist`
+  MODIFY `artist_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `playlists`
+--
+ALTER TABLE `playlists`
+  MODIFY `playlist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `song`
+--
+ALTER TABLE `song`
+  MODIFY `song_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `albums`
+--
+ALTER TABLE `albums`
+  ADD CONSTRAINT `album_artist_id` FOREIGN KEY (`artist_id`) REFERENCES `artist` (`artist_id`);
+
+--
+-- Constraints for table `liked_songs`
+--
+ALTER TABLE `liked_songs`
+  ADD CONSTRAINT `song_ids` FOREIGN KEY (`song_id`) REFERENCES `song` (`song_id`),
+  ADD CONSTRAINT `user_ids` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `playlists`
+--
+ALTER TABLE `playlists`
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `playlist_songs`
+--
+ALTER TABLE `playlist_songs`
+  ADD CONSTRAINT `playlist_id` FOREIGN KEY (`playlist_id`) REFERENCES `playlists` (`playlist_id`),
+  ADD CONSTRAINT `song_id` FOREIGN KEY (`song_id`) REFERENCES `song` (`song_id`);
+
+--
+-- Constraints for table `song`
+--
+ALTER TABLE `song`
+  ADD CONSTRAINT `album_id` FOREIGN KEY (`album_id`) REFERENCES `albums` (`album_id`),
+  ADD CONSTRAINT `artist_id` FOREIGN KEY (`artist_id`) REFERENCES `artist` (`artist_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ VibeNest/db.cjs
@@ -0,0 +1,21 @@
+
+const mysql = require('mysql2');
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: 'localhost',       // Your database host
+  user: 'root',            // Your database username
+  password: 'root',    // Your database password
+  database: 'music-streaming-platform',
+   
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected to the database as id ' + connection.threadId);
+});
+
+module.exports = connection;
